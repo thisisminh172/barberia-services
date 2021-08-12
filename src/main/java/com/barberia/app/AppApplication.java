d4ea9efeb65f23d0aefb0e1e94b5bbf3b64f68a6
thisisminh172 <thisisminh172@gmail.com>
2021-08-12 10:18:53 +0700

    deploy

--- src/main/java/com/barberia/app/AppApplication.java
+++ src/main/java/com/barberia/app/AppApplication.java
@@ -3,16 +3,22 @@ package com.barberia.app;
 import com.barberia.app.files.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
 		FileStorageProperties.class
 })
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AppApplication.class);
+	}
 }
